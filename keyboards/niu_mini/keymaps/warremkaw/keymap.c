a4ac4a33975a60375d3a5426e6328deba87539a0
warremkaw <janssenswarre@hotmail.com>
2019-11-19 19:54:05 +0100

    add del to lwr

--- keyboards/niu_mini/keymaps/warremkaw/keymap.c
+++ keyboards/niu_mini/keymaps/warremkaw/keymap.c
@@ -25,11 +25,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
         KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_SFTENT, \
-        KC_LCTL, MED,    KC_LALT, KC_LGUI, LWR,     KC_SPC,  KC_SPC,  RSE,     KC_RGUI, KC_0,    ARW,     KC_RCTL 
+        KC_LCTL, MED,     KC_LALT, KC_LGUI, LWR,     KC_SPC,  KC_SPC,  RSE,     KC_RGUI, KC_0,    ARW,     KC_RCTL 
     ),
 
     [_LWR] = LAYOUT_ortho_4x12(
-        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  _______, \
+        KC_GRV,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_MINS, KC_EQL,  KC_DEL, \
         XXXXXXX, XXXXXXX, XXXXXXX, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_LCBR, KC_RCBR, KC_PIPE, KC_BSLS, XXXXXXX, \
         _______, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ 
