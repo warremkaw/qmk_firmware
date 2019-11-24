74bdf37eb1b5bf277d477a1b29ca30350253f192
warremkaw <janssenswarre@hotmail.com>
2019-11-24 11:41:34 +0100

    add config.h for no reason :)

---
+++ keyboards/niu_mini/keymaps/warremkaw/config.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "config_common.h"
+
+
+/* USB Device descriptor parameter */
+#undef MANUFACTURER
+#define MANUFACTURER    warremkaw
+#undef DESCRIPTION
+#define DESCRIPTION     Ortholinear awesomeness!
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
+//#define NO_ACTION_MACRO
+//#define NO_ACTION_FUNCTION
